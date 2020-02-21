52941c1143a16743fdb92d6f1b49eeaccbb8c713
Dominick Licciardi <dominick.nola@gmail.com>
2020-02-20 20:19:19 -0700

    Tiger Color Picker

--- app/src/main/java/com/tutorialkart/colorpicker/MainActivity.kt
+++ app/src/main/java/com/tutorialkart/colorpicker/MainActivity.kt
@@ -3,18 +3,27 @@ package com.tutorialkart.colorpicker
 import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.colorpicker.*
-import android.text.Editable
-import android.text.TextWatcher
-
+//import sun.jvm.hotspot.utilities.IntArray
 
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val settings = getSharedPreferences("settings", 0)
+        val editor = settings.edit()
+        val settings2 = getSharedPreferences("settings2", 0)
+        val editor2 = settings2.edit()
+        val settings3 = getSharedPreferences("settings3", 0)
+        val editor3 = settings3.edit()
+        val settings4 = getSharedPreferences("settings4", 0)
+        val editor4 = settings4.edit()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -61,6 +70,10 @@ class MainActivity : Activity() {
                 val colorStr = getColorString()
                 strColor.setText(colorStr.replace("#","").toUpperCase())
                 btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
+
+                textView11.text = progress.toString()
+
+
             }
         })
 
@@ -73,6 +86,9 @@ class MainActivity : Activity() {
                 val colorStr = getColorString()
                 strColor.setText(colorStr.replace("#","").toUpperCase())
                 btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
+
+                textView8.text = progress.toString()
+
             }
         })
 
@@ -85,6 +101,9 @@ class MainActivity : Activity() {
                 val colorStr = getColorString()
                 strColor.setText(colorStr.replace("#","").toUpperCase())
                 btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
+
+                textView9.text = progress.toString()
+
             }
         })
 
@@ -97,6 +116,9 @@ class MainActivity : Activity() {
                 val colorStr = getColorString()
                 strColor.setText(colorStr.replace("#","").toUpperCase())
                 btnColorPreview.setBackgroundColor(Color.parseColor(colorStr))
+
+                textView10.text = progress.toString()
+
             }
         })
 
@@ -108,6 +130,82 @@ class MainActivity : Activity() {
             val color:String = getColorString()
             btnColorSelected.setBackgroundColor(Color.parseColor(color))
             colorSelector.visibility = View.GONE
+        }
+
+        // first save button
+        button11.setOnClickListener {
+
+            editor.putInt("Red", colorR.progress)
+            editor.putInt("Green", colorG.progress)
+            editor.putInt("Blue", colorR.progress)
+            editor.putInt("Opacity", colorG.progress)
+            editor.apply()
+        }
+
+        // first preset button
+        button10.setOnClickListener {
+
+            colorR.progress = settings.getInt("Red", 0)
+            colorG.progress = settings.getInt("Green", 0)
+            colorB.progress = settings.getInt("Blue", 0)
+            colorA.progress = settings.getInt("Opacity", 0)
+        }
+
+        // second save button
+        button12.setOnClickListener {
+
+            editor2.putInt("Red", colorR.progress)
+            editor2.putInt("Green", colorG.progress)
+            editor2.putInt("Blue", colorR.progress)
+            editor2.putInt("Opacity", colorG.progress)
+            editor2.apply()
+        }
+
+        // second preset button
+        button9.setOnClickListener {
+
+            colorR.progress = settings2.getInt("Red", 0)
+            colorG.progress = settings2.getInt("Green", 0)
+            colorB.progress = settings2.getInt("Blue", 0)
+            colorA.progress = settings2.getInt("Opacity", 0)
+        }
+
+        // third save button
+        button13.setOnClickListener {
+
+            editor3.putInt("Red", colorR.progress)
+            editor3.putInt("Green", colorG.progress)
+            editor3.putInt("Blue", colorR.progress)
+            editor3.putInt("Opacity", colorG.progress)
+            editor3.apply()
+        }
+
+        // third preset button
+        button8.setOnClickListener {
+
+            colorR.progress = settings3.getInt("Red", 0)
+            colorG.progress = settings3.getInt("Green", 0)
+            colorB.progress = settings3.getInt("Blue", 0)
+            colorA.progress = settings3.getInt("Opacity", 0)
+        }
+
+        // forth save button
+        button14.setOnClickListener {
+
+            editor4.putInt("Red", colorR.progress)
+            editor4.putInt("Green", colorG.progress)
+            editor4.putInt("Blue", colorR.progress)
+            editor4.putInt("Opacity", colorG.progress)
+            editor4.apply()
+        }
+
+        // forth preset button
+        button7.setOnClickListener {
+
+            colorR.progress = settings4.getInt("Red", 0)
+            colorG.progress = settings4.getInt("Green", 0)
+            colorB.progress = settings4.getInt("Blue", 0)
+            colorA.progress = settings4.getInt("Opacity", 0)
         }
     }
     fun getColorString(): String {
