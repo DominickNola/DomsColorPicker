b631015a4ab93a0be26e66f63040e6a0351ed7aa
Dominick Licciardi <dominick.nola@gmail.com>
2020-02-23 19:59:33 -0700

    fixed back button

--- app/src/main/java/com/tutorialkart/colorpicker/MainActivity.kt
+++ app/src/main/java/com/tutorialkart/colorpicker/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.tutorialkart.colorpicker
 
-import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -8,10 +7,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.widget.SeekBar
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.colorpicker.*
-import kotlinx.android.synthetic.main.colorpicker.view.*
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -144,13 +140,6 @@ class MainActivity : AppCompatActivity() {
         // first save button
         button11.setOnClickListener {
 
-            // add color from the seekBars to the first settings/editor
-            editor.putInt("Red", colorR.progress)
-            editor.putInt("Green", colorG.progress)
-            editor.putInt("Blue", colorB.progress)
-            editor.putInt("Opacity", colorA.progress)
-            editor.apply()
-
             editText2.visibility = View.VISIBLE
             button7.visibility = View.GONE
             button8.visibility = View.GONE
@@ -177,11 +166,22 @@ class MainActivity : AppCompatActivity() {
             done2.visibility = View.GONE
             done3.visibility = View.GONE
             done4.visibility = View.GONE
+            back1.visibility = View.VISIBLE
+            back2.visibility = View.GONE
+            back3.visibility = View.GONE
+            back4.visibility = View.GONE
 
         }
 
         // first done button
         done1.setOnClickListener {
+            // add color from the seekBars to the first settings/editor
+            editor.putInt("Red", colorR.progress)
+            editor.putInt("Green", colorG.progress)
+            editor.putInt("Blue", colorB.progress)
+            editor.putInt("Opacity", colorA.progress)
+            editor.apply()
+
             button10.text = editText2.text
             color_name_edit.text = editText2.text
             editor5.putString("buttons", button10.text.toString())
@@ -210,7 +210,38 @@ class MainActivity : AppCompatActivity() {
             textView11.visibility = View.VISIBLE
             color_name_edit.visibility = View.VISIBLE
             done1.visibility = View.GONE
+            back1.visibility = View.GONE
 
+        }
+
+        back1.setOnClickListener {
+
+            button10.text = buttons.getString("buttons", "default")
+            color_name_edit.text = buttons.getString("buttons", "default")
+            editText2.visibility = View.GONE
+            button7.visibility = View.VISIBLE
+            button8.visibility = View.VISIBLE
+            button9.visibility = View.VISIBLE
+            button10.visibility = View.VISIBLE
+            button11.visibility = View.VISIBLE
+            button12.visibility = View.VISIBLE
+            button13.visibility = View.VISIBLE
+            button14.visibility = View.VISIBLE
+            colorR.visibility = View.VISIBLE
+            colorG.visibility = View.VISIBLE
+            colorB.visibility = View.VISIBLE
+            colorA.visibility = View.VISIBLE
+            textView2.visibility = View.VISIBLE
+            textView3.visibility = View.VISIBLE
+            textView4.visibility = View.VISIBLE
+            textView5.visibility = View.VISIBLE
+            textView8.visibility = View.VISIBLE
+            textView9.visibility = View.VISIBLE
+            textView10.visibility = View.VISIBLE
+            textView11.visibility = View.VISIBLE
+            color_name_edit.visibility = View.VISIBLE
+            done1.visibility = View.GONE
+            back1.visibility = View.GONE
         }
 
         // second preset button
@@ -228,12 +259,6 @@ class MainActivity : AppCompatActivity() {
 
         // second save button
         button12.setOnClickListener {
-
-            editor2.putInt("Red", colorR.progress)
-            editor2.putInt("Green", colorG.progress)
-            editor2.putInt("Blue", colorB.progress)
-            editor2.putInt("Opacity", colorA.progress)
-            editor2.apply()
 
             editText3.visibility = View.VISIBLE
             button7.visibility = View.GONE
@@ -261,10 +286,20 @@ class MainActivity : AppCompatActivity() {
             done2.visibility = View.VISIBLE
             done3.visibility = View.GONE
             done4.visibility = View.GONE
+            back1.visibility = View.GONE
+            back2.visibility = View.VISIBLE
+            back3.visibility = View.GONE
+            back4.visibility = View.GONE
         }
 
         // second done button
         done2.setOnClickListener {
+            editor2.putInt("Red", colorR.progress)
+            editor2.putInt("Green", colorG.progress)
+            editor2.putInt("Blue", colorB.progress)
+            editor2.putInt("Opacity", colorA.progress)
+            editor2.apply()
+
             button9.text = editText3.text
             color_name_edit.text = editText3.text
             editor6.putString("buttons", button9.text.toString())
@@ -293,7 +328,38 @@ class MainActivity : AppCompatActivity() {
             textView11.visibility = View.VISIBLE
             color_name_edit.visibility = View.VISIBLE
             done2.visibility = View.GONE
+            back2.visibility = View.GONE
 
+        }
+
+        back2.setOnClickListener {
+
+            button9.text = buttons2.getString("buttons", "default")
+            color_name_edit.text = buttons2.getString("buttons", "default")
+            editText3.visibility = View.GONE
+            button7.visibility = View.VISIBLE
+            button8.visibility = View.VISIBLE
+            button9.visibility = View.VISIBLE
+            button10.visibility = View.VISIBLE
+            button11.visibility = View.VISIBLE
+            button12.visibility = View.VISIBLE
+            button13.visibility = View.VISIBLE
+            button14.visibility = View.VISIBLE
+            colorR.visibility = View.VISIBLE
+            colorG.visibility = View.VISIBLE
+            colorB.visibility = View.VISIBLE
+            colorA.visibility = View.VISIBLE
+            textView2.visibility = View.VISIBLE
+            textView3.visibility = View.VISIBLE
+            textView4.visibility = View.VISIBLE
+            textView5.visibility = View.VISIBLE
+            textView8.visibility = View.VISIBLE
+            textView9.visibility = View.VISIBLE
+            textView10.visibility = View.VISIBLE
+            textView11.visibility = View.VISIBLE
+            color_name_edit.visibility = View.VISIBLE
+            done2.visibility = View.GONE
+            back2.visibility = View.GONE
         }
 
         // third preset button
@@ -310,12 +376,6 @@ class MainActivity : AppCompatActivity() {
 
         // third save button
         button13.setOnClickListener {
-
-            editor3.putInt("Red", colorR.progress)
-            editor3.putInt("Green", colorG.progress)
-            editor3.putInt("Blue", colorB.progress)
-            editor3.putInt("Opacity", colorA.progress)
-            editor3.apply()
 
             editText4.visibility = View.VISIBLE
             button7.visibility = View.GONE
@@ -343,10 +403,21 @@ class MainActivity : AppCompatActivity() {
             done2.visibility = View.GONE
             done3.visibility = View.VISIBLE
             done4.visibility = View.GONE
+            back1.visibility = View.GONE
+            back2.visibility = View.GONE
+            back3.visibility = View.VISIBLE
+            back4.visibility = View.GONE
         }
 
         // third done button
         done3.setOnClickListener {
+
+            editor3.putInt("Red", colorR.progress)
+            editor3.putInt("Green", colorG.progress)
+            editor3.putInt("Blue", colorB.progress)
+            editor3.putInt("Opacity", colorA.progress)
+            editor3.apply()
+
             button8.text = editText4.text
             color_name_edit.text = editText4.text
             editor7.putString("buttons", button8.text.toString())
@@ -375,7 +446,38 @@ class MainActivity : AppCompatActivity() {
             textView11.visibility = View.VISIBLE
             color_name_edit.visibility = View.VISIBLE
             done3.visibility = View.GONE
+            back3.visibility = View.GONE
 
+        }
+
+        back3.setOnClickListener {
+
+            button8.text = buttons3.getString("buttons", "default")
+            color_name_edit.text = buttons3.getString("buttons", "default")
+            editText4.visibility = View.GONE
+            button7.visibility = View.VISIBLE
+            button8.visibility = View.VISIBLE
+            button9.visibility = View.VISIBLE
+            button10.visibility = View.VISIBLE
+            button11.visibility = View.VISIBLE
+            button12.visibility = View.VISIBLE
+            button13.visibility = View.VISIBLE
+            button14.visibility = View.VISIBLE
+            colorR.visibility = View.VISIBLE
+            colorG.visibility = View.VISIBLE
+            colorB.visibility = View.VISIBLE
+            colorA.visibility = View.VISIBLE
+            textView2.visibility = View.VISIBLE
+            textView3.visibility = View.VISIBLE
+            textView4.visibility = View.VISIBLE
+            textView5.visibility = View.VISIBLE
+            textView8.visibility = View.VISIBLE
+            textView9.visibility = View.VISIBLE
+            textView10.visibility = View.VISIBLE
+            textView11.visibility = View.VISIBLE
+            color_name_edit.visibility = View.VISIBLE
+            done3.visibility = View.GONE
+            back3.visibility = View.GONE
         }
 
         // forth preset button
@@ -393,12 +495,6 @@ class MainActivity : AppCompatActivity() {
 
         // forth save button
         button14.setOnClickListener {
-
-            editor4.putInt("Red", colorR.progress)
-            editor4.putInt("Green", colorG.progress)
-            editor4.putInt("Blue", colorB.progress)
-            editor4.putInt("Opacity", colorA.progress)
-            editor4.apply()
 
             editText5.visibility = View.VISIBLE
             button7.visibility = View.GONE
@@ -426,11 +522,22 @@ class MainActivity : AppCompatActivity() {
             done2.visibility = View.GONE
             done3.visibility = View.GONE
             done4.visibility = View.VISIBLE
+            back1.visibility = View.GONE
+            back2.visibility = View.GONE
+            back3.visibility = View.GONE
+            back4.visibility = View.VISIBLE
 
         }
 
         // forth done button
         done4.setOnClickListener {
+
+            editor4.putInt("Red", colorR.progress)
+            editor4.putInt("Green", colorG.progress)
+            editor4.putInt("Blue", colorB.progress)
+            editor4.putInt("Opacity", colorA.progress)
+            editor4.apply()
+
             button7.text = editText5.text
             color_name_edit.text = editText5.text
             editor8.putString("buttons", button7.text.toString())
@@ -459,11 +566,40 @@ class MainActivity : AppCompatActivity() {
             textView11.visibility = View.VISIBLE
             color_name_edit.visibility = View.VISIBLE
             done4.visibility = View.GONE
+            back4.visibility = View.GONE
 
         }
+
+        back4.setOnClickListener {
+
+            button7.text = buttons4.getString("buttons", "default")
+            color_name_edit.text = buttons4.getString("buttons", "default")
+            editText5.visibility = View.GONE
+            button7.visibility = View.VISIBLE
+            button8.visibility = View.VISIBLE
+            button9.visibility = View.VISIBLE
+            button10.visibility = View.VISIBLE
+            button11.visibility = View.VISIBLE
+            button12.visibility = View.VISIBLE
+            button13.visibility = View.VISIBLE
+            button14.visibility = View.VISIBLE
+            colorR.visibility = View.VISIBLE
+            colorG.visibility = View.VISIBLE
+            colorB.visibility = View.VISIBLE
+            colorA.visibility = View.VISIBLE
+            textView2.visibility = View.VISIBLE
+            textView3.visibility = View.VISIBLE
+            textView4.visibility = View.VISIBLE
+            textView5.visibility = View.VISIBLE
+            textView8.visibility = View.VISIBLE
+            textView9.visibility = View.VISIBLE
+            textView10.visibility = View.VISIBLE
+            textView11.visibility = View.VISIBLE
+            color_name_edit.visibility = View.VISIBLE
+            done4.visibility = View.GONE
+            back4.visibility = View.GONE
+        }
     }
-
-
 
     fun getColorString(): String {
         var a = Integer.toHexString(((255*colorA.progress)/colorA.max))
